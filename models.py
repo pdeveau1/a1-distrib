Update epoch, learning rate, and L2 params: =====Train Accuracy===== Accuracy: 6841 / 6920 = 0.988584 Precision (fraction of predicted positives that are correct): 3566 / 3601 = 0.990280; Recall (fraction of true positives predicted correctly): 3566 / 3610 = 0.987812; F1 (harmonic mean of precision and recall): 0.989045 =====Dev Accuracy===== Accuracy: 674 / 872 = 0.772936 Precision (fraction of predicted positives that are correct): 352 / 458 = 0.768559; Recall (fraction of true positives predicted correctly): 352 / 444 = 0.792793; F1 (harmonic mean of precision and recall): 0.780488 Time for training and evaluation: 8.15 seconds

models.py
@@ -159,7 +159,7 @@ class LogisticRegressionClassifier(SentimentClassifier):
         for idx, count in features.items():
             if idx >= len(self.weights):
                 self.weights = np.append(self.weights, np.zeros(idx - len(self.weights) + 1))
-            self.weights[idx] += learning_rate * (error * count - 0.01 * self.weights[idx])  # L2 regularization
+            self.weights[idx] += learning_rate * (error * count - 0.001 * self.weights[idx])  # L2 regularization
 
 def train_perceptron(train_exs: List[SentimentExample], feat_extractor: FeatureExtractor, num_epochs: int=5) -> PerceptronClassifier:
     """
@@ -187,7 +187,7 @@ def train_perceptron(train_exs: List[SentimentExample], feat_extractor: FeatureE
     return model
 
 
-def train_logistic_regression(train_exs: List[SentimentExample], feat_extractor: FeatureExtractor, num_epochs: int=20, learning_rate: float=0.2) -> LogisticRegressionClassifier:
+def train_logistic_regression(train_exs: List[SentimentExample], feat_extractor: FeatureExtractor, num_epochs: int=20, learning_rate: float=0.05) -> LogisticRegressionClassifier:
     """
     Train a logistic regression model.
     :param train_exs: training set, List of SentimentExample objects